ff73dcdb1ed6f2029e2e72ee8296458373d7ba2f
Nicholas Cass <nickcass88@gmail.com>
2021-09-01 19:19:36 +1000

    Adding AudioProcessing project

---
+++ AudioProcessing/AudioRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioProcessing
+{
+    class AudioRenderer
+    {
+        private float Amplitude = 1f;
+        private float Frequency = 60f;
+        private float Phase = 0f;
+        private float Offset = 0f;
+        private float Invert = 1;
+
+        public AudioRenderer(float f = 60f, float a = 1f, float p = 0f, float o = 0f, float i = 1f)
+        {
+            Amplitude = a;
+            Frequency = f;
+            Phase = p;
+            Offset = o;
+            Invert = i;
+        }
+
+        public AudioRenderer()
+        { }
+        
+        public float GetPoint(float t)
+        {
+            float output = 0f;
+            float angFrequency = 2 * (float)Math.PI * Frequency;
+            output = Amplitude * (float)Math.Sin((angFrequency * t) + Phase);
+
+
+            return output;
+        }
+        public float GetPointRelative(float t)
+        {
+            float angFrequency = 2 * (float)Math.PI * Frequency;
+
+            return (float)Math.Sin((angFrequency * t) + Phase); ;
+        }
+
+    }
+}
---
+++ AudioProcessing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AudioProcessing
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            AudioRenderer render = new AudioRenderer(256f);
+            Window window = new Window();
+            //float increment = 0.01;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(render.GetPointRelative((float)i));
+            }
+           
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+
+            window.Resize += (sender, args) =>
+            {
+                window.Buffer = BufferedGraphicsManager.Current.Allocate(window.Graphics, new Rectangle(0, 0, window.Width, window.Height));
+                window.Buffer.Graphics.Clear(Color.Black);
+                window.Update();
+
+                for (int x = 0; x < window.Width - 1; x++)
+                {
+                    float y = render.GetPointRelative(x) * (window.Height / 2) + (window.Height / 2);
+                    float y2 = render.GetPointRelative(x + 1) * (window.Height / 2) + (window.Height / 2);
+                    window.Buffer.Graphics.DrawLine(new Pen(Color.Red, 1f), x, y, x + 1, y2);
+                }
+            };
+
+            while (!window.IsDisposed)
+            {
+                window.Update();
+            }
+
+        }
+    }
+}
